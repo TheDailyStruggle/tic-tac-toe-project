9a8b9f7b9c3794c811593f12dc34f6195223fbad
TheDailyStruggle <chadmontgomery46@gmail.com>
2023-06-13 17:04:18 -0700

    Add check win logic

--- javascript.js
+++ javascript.js
@@ -16,7 +16,6 @@ const gameBoard = (() => {
             square.id = i.toString();
             square.innerText = board[i];
             square.addEventListener("click", e => {
-                console.log(e);
                 let index = e.target.id;
                 let player = currentPlayer.symbol;
                 makeMove(index, player);
@@ -29,6 +28,7 @@ const gameBoard = (() => {
         if (index >= 0 && index < board.length && board[index] === "") {
             board[index] = player;
             displayBoard();
+            checkWin(gameBoard.getBoard(), currentPlayer.symbol);
             togglePlayer();
             return true;
         }
@@ -76,4 +76,30 @@ let currentPlayer = p1;
 const togglePlayer = function () {
     currentPlayer = currentPlayer === p1 ? p2 : p1;
     return currentPlayer;
+};
+
+
+// Check Win
+
+const checkWin = (board, symbol) => {
+
+    const winConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    for (let condition of winConditions) {
+        const [a, b, c] = condition;
+        if (board[a] === symbol && board[b] === symbol && board[c] === symbol) {
+            // display win
+            console.log(`${symbol} Wins!`)
+        }
+    };
+
 };
